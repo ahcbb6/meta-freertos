dbfee5f445464fc087268c1a9ee3fd6522bdfc73
Alejandro Enedino Hernandez Samaniego <alejandro@enedino.org>
2021-03-25 16:10:49 -0600

    freertos_periodic: Add comment about cmd_finished to the testcase
    Signed-off-by: Alejandro Enedino Hernandez Samaniego <alejandro@enedino.org>

--- lib/oeqa/runtime/cases/freertos_periodic.py
+++ lib/oeqa/runtime/cases/freertos_periodic.py
@@ -8,13 +8,15 @@ from oeqa.runtime.case import OERuntimeTestCase
 from oeqa.core.decorator.oetimeout import OETimeout
 import re
 
+# This needs the cmd_finished boot pattern set to something like "secs"
+
 class FreeRTOSTest(OERuntimeTestCase):
 
     @OETimeout(15)
     def test_freertos_periodic(self):
         # Send empty string which allows us to read the output of the RTOS serial console
         cmd = ''
-        # Look for Periodic task which run every 10 seconds, so there
+        # Look for Periodic task which runs every 10 seconds, so there
         # should be one printed on the serial console since the timeout is 15
         pattern = 'Periodic task 10 secs'
         # Send it raw, so it does not look for echo $?, it just looks at the output
