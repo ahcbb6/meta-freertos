b9f83f4c0fd1692241402fa8f00aaccdceb90c8d
Alejandro Enedino Hernandez Samaniego <alejandro@enedino.org>
2021-05-29 23:58:22 -0600

    freertos_periodic test case: Improve testcase
    By passing timeout to the run_serial function we can achieve
    the same functionality of the test case without having to
    override TESTIMAGE_BOOT_PATTERNS[search_cmd_finished].
    
    The only requirement is that the following statement holds true:
    
    10 secs < run_serial timeout < test timeout
    
    This allows us to properly read the test output in time without
    having to override the previously mentioned pattern.
    
    Signed-off-by: Alejandro Enedino Hernandez Samaniego <alejandro@enedino.org>

--- lib/oeqa/runtime/cases/freertos_periodic.py
+++ lib/oeqa/runtime/cases/freertos_periodic.py
@@ -8,8 +8,6 @@ from oeqa.runtime.case import OERuntimeTestCase
 from oeqa.core.decorator.oetimeout import OETimeout
 import re
 
-# This needs the cmd_finished boot pattern set to something like "secs"
-
 class FreeRTOSTest(OERuntimeTestCase):
 
     @OETimeout(15)
@@ -19,7 +17,13 @@ class FreeRTOSTest(OERuntimeTestCase):
         # Look for Periodic task which runs every 10 seconds, so there
         # should be one printed on the serial console since the timeout is 15
         pattern = 'Periodic task 10 secs'
+
         # Send it raw, so it does not look for echo $?, it just looks at the output
-        status, output = self.target.runner.run_serial(cmd,raw=True)
+
+        # The following statement needs to be True:
+        # 10 secs < run_serial timeout < test timeout
+        # This will cause the run_serial function to return before the test times out,
+        # but it will have run long enough for the pattern to be found in its output.
+        status, output = self.target.runner.run_serial(cmd,raw=True, timeout=12)
         match = bool(re.search(pattern,output))
         self.assertEqual(match, True)
